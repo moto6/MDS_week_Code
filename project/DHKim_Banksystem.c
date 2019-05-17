e511ef3168e9fb312e2aaa2233388a486184df92
MDS_KDH <ultraraptor@naver.com>
2019-05-17 17:23:27 +0900

    project

--- project/DHKim_Banksystem.c
+++ project/DHKim_Banksystem.c
@@ -2,10 +2,12 @@
 #define WAITKEY_RUTIN do{printf("\ncontinue to press any key");fflush(stdin);getc(stdin);}while(0);
 #define MID_DBL_LINE do{puts("=====================================================");}while(0);
 #define MAXLEN 100 //max len of buf
+#define WAIT_RUTIN do{puts("Looding..");sleep(1);puts("Looding......");sleep( 1 );}while(0);
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void disp_calendar(int year, int month);
 int get_weekday(int year, int month);
@@ -128,30 +130,12 @@ int loginout (void) {
 	int idok=0, pwok=0;
 	char buf[MAXLEN];
 	char ch;
+
 	
-	puts("======admin ID / Passward Check=====");
-	printf("ID : ");
-	scanf("%s",&buf);
-	if( loginflag == 0 )
-		if( 0 == strcmp(ID_admin,buf) ) {
-			printf("PW : ");
-			scanf("%s",&buf);
-			if( 0 == strcmp(PW_admin,buf) ) {
-				MID_DBL_LINE 
-				puts("administor loginned!! WEEEELLLLLCOME");
-				MID_DBL_LINE 
-				loginflag=1;	
-				WAITKEY_RUTIN
-			}
-		}
-		else {
-			puts("ID is wrong!");
-			WAITKEY_RUTIN
-			return;
-		}
-	else if( loginflag == 1) {
+	if( loginflag == 1) {
 		puts("already admin login");
 		printf("do you want Logout? (Y/N)");
+		fflush(stdin);
 		scanf("%c",&ch);
 		if(ch == 'Y' || ch == 'y') {
 			puts("outout! bye bye");
@@ -163,22 +147,57 @@ int loginout (void) {
 		}
 	}
 	
+
+	
+	puts("======admin ID / Passward Check=====");
+	puts("defulat : admin/admin") ;
+	printf("ID : ");
+	scanf("%s",&buf);
+	
+		if( 0 == strcmp(ID_admin,buf) ) {
+			printf("PW : ");
+			scanf("%s",&buf);
+			if( 0 == strcmp(PW_admin,buf) ) {
+				MID_DBL_LINE 
+				puts("administor loginned!! WEEELLLCOME");
+				MID_DBL_LINE 
+				loginflag=1;	
+				WAIT_RUTIN
+				WAITKEY_RUTIN
+			}
+		}
+		else {
+			puts("ID is wrong!");
+			WAITKEY_RUTIN
+			return;
+		}
+	
 		
 	
 }
 
 int accprint (void) {
 	int i=0;
-	
-	for(i=0;i<10;i++) {//
-		puts("구조체포인터를 넘겨받아 계좌정보를 출력하는 함수 구현");
+	if(loginflag) {
+		for(i=0;i<5;i++) {//
+			puts("구조체포인터를 넘겨받아 계좌정보를 출력");
 			//print_accinfo(/*acc 구조체 주소*/);
 			
+		}
+		MID_DBL_LINE
+		puts("end of the Lisht") ;
+		MID_DBL_LINE
+		WAITKEY_RUTIN
+		//당신의 잔액, 송금계좌, 금액 
 	}
-	MID_DBL_LINE
-	puts("\nend of the Lisht") ;
-	MID_DBL_LINE
-	WAITKEY_RUTIN
+	else {
+		puts("Login First!");
+		WAITKEY_RUTIN
+	}
+
+	
+	
+	
 }
 
 int banktrns (void) {
@@ -215,7 +234,14 @@ int depositm (void) {
 }
 
 int checkdps (void) {
-	
+		if(loginflag) {
+		printf("ID : %s",cur_user.ID);//기능 추가구현 필요 
+		printf("deposit : %d",9999999);
+	}
+	else {
+		puts("Login First!");
+		WAITKEY_RUTIN
+	}
 }
 
 int calprint (void) {
@@ -334,262 +360,3 @@ void disp_calendar(int year, int month)
 
 }
 
-
-
-/*
-
-int get_checkRange(char* what, int min, int max) {
-	int input = 0;
-	printf("> %s(%d-%d) : ", what, min, max); 
-	scanf("%d", &input);
-	if ((input < min) || (input > max))     // 허용 범위의 숫자가 아니면 반환
-	{
-		printf("@ 유효한 번호가 아닙니다!\n");
-		return -1;
-	}
-	return input;
-}
-
-void deleteFromIDList(int dno) {
-	int i=0;
-
-	for (i = dno; i < (e_cnt - 1); i++)
-	{
-		e_num[i] = e_num[i + 1];
-		e_pwd[i] = e_pwd[i + 1];
-	}
-	e_cnt--;										// 등록된 사번 수 감소
-}
-
-void insertToIDList(int id, int pwd) {
-	e_num[e_cnt] = id;
-	e_pwd[e_cnt] = pwd;
-	e_cnt++;                                   // 등록된 사번 수 증가
-	printf("# %d번이 등록되었습니다!\n", id);
-}
-
-void printIDList(){
-	int i=0;
-
-	printf("\n>> 현재 등록된 사번(비밀번호) : ");
-	for (i = 0; i < e_cnt; i++)
-	{
-		printf("%5d(%d)", e_num[i], e_pwd[i]);
-	}
-	printf("\n\n");
-}
-
-
-void deleteID(int dno) {
-	char yn = 0;
-
-	printf("@ 이미 등록되었습니다!\n");
-	printf("@ 삭제 하시겠습니까(Y/N)?"); 
-
-	fflush(stdin);
-	scanf("%c", &yn);
-	if ((yn == 'Y') || (yn == 'y'))
-	{
-		deleteFromIDList(dno);
-		printf("@ %d번이 삭제 되었습니다!\n", e_num[dno]);
-		printIDList();
-	}
-	printf("@ 취소 되었습니다!\n");
-}
-
-void insertID(int id){
-	int pwd = 0;
-	int cnt =  sizeof(e_num) / sizeof(e_num[0]);    	// 최대 등록인원 수 계산
-	if (e_cnt == cnt)                          						// 등록인원 수가 최대 인원 수와 같으면 반환
-	{
-		printf("@ 등록 인원이 초과되었습니다!");
-		return;
-	}
-	pwd = get_checkRange("비밀번호", 0, 9999);
-	if (pwd == 0)  return;
-	insertToIDList(id, pwd);
-	printIDList();
-}
-
-// 사번 등록 / 삭제
-void enroll_num(void)//1:등록or 삭제 함수 
-{
-	int num,pwd;                          			// 사번, 비밀번호 
-	int index;                                  // 사번이 있는 배열의 위치
-	int buf=0;
-	int ch=0;
-
-	if (login_num != 0)                   // 이미 로그인 된 상태면 반환
-	{
-		printf("@ 로그아웃 후 사용할 수 있습니다!\n");
-		return;
-	}
-	else {
-	
-		printf("\n[ 사번 등록/삭제 ]\n\n");
-	
-	    // todo: 사번 등록 / 삭제 코드 작성
-	    printf("> 사번(1-9999) :");
-	    scanf("%d", &num);
-		
-		if(is_enroll_num(num)) {//사번이 기 등록여부 확인 등록되있으면 1 없으면 0 
-				deleteID(1);
-			//삭제여부를 묻는 코드까지 들어있음 scanf yn코드 다있음. 
-			
-		}
-		else {
-			printf("> 비밀번호(0-9999) :");
-		    scanf("%d", &pwd);
-		    printf("# %d번이 등록되었습니다!\n\n",num);
-		    printf(" 현재 등록된 사번(비밀번호)  :  %d (%d)", num  , pwd);
-		    insertToIDList(num,pwd);
-		    printf("e_cnt : %d",e_cnt);
-		}
-	}
-	return;
-	
-}
-
-// 로그인 / 로그아웃
-void login_out(void)
-{
-	int num, pwd;
-	int index;
-	
-	if(login_num) {//로그인상태?
-		puts("# 로그아웃 되었습니다!");
-		login_num = 0;
-		return; 
-	}
-	
-	else {//로그인 안된 경우 들어오는 코드 
-		 
-		puts("\n\r[ 로그인 ] \n\r");
-		printf("> 사번 :");
-		scanf("%d",&num); 
-		printf("> 비밀번호 :");
-		scanf("%d",&pwd); 
-		
-		//is_vaild_accunt(num,pwd) 1=유효,0=무효
-		if( is_vaild_accunt(num,pwd) ) {//전체 사번에서 입력된 번호를 확인 
-			
-		//todo 로그인 플래그  
-			login_num = num;
-			puts("# 로그인 되었습니다!");
-		}
-		else{
-			puts(" 비밀번호가 틀렸습니다! ");
-		}
-	 
-	}
-	//로그인 체크 
-	
-
-}
-
-// 등록된 번호인지 확인 등록되있으면 1 없으면 0 
-int is_enroll_num(int num)
-{
-	int i;
-	
-	for(i=0;i<e_cnt;i++) {
-		if(num==e_num[i]) {
-			return 1;
-		}
-		else {
-			
-		} 
-		
-	}
-	// todo: 등록된 번호인지 확인 코드 작성
-
-	return 0;
-}
-
-
-//int is_vaild_accunt(int num,int pwd);//함수 추가함 1=유효,0=무효
-int is_vaild_accunt(int num,int pwd)
-{
-	int i;
-	puts("\r\nis_vaild_accunt\r\n");
-	
-	for(i=0 ; i<(e_cnt+2) ; i++) {
-		if(e_num == num && e_pwd == pwd) {
-			return 1;
-		}
-		else {}
-		
-	puts("\r\nis_vaild_accunt run loop\r\n");
-	}
-	
-	
-	return 0;
-}
-
-// 회의실 예약 등록
-void input_schedule(void)
-{
-	int year, month, day;    // 스케줄을 입력할 날
-
-	if (login_num == 0)      // 로그인되지 않았다면 반환
-	{
-		printf("@ 로그인 후 사용할 수 있습니다!\n");
-		return;
-	}
-
-	printf("\n[ 회의실 예약 ]\n\n");
-
-	printf("> 년, 월 입력 : ");
-	scanf("%d%d", &year, &month);
-	// todo: 회의실 예약 등록 코드 작성
-
-
-
-
-
-	printf("@ 회의실 예약이 등록되었습니다!\n");
-
-}
-
-// 해당 날짜에 대한 스케줄 수 확인, 없는 경우 0 반환
-int on_schedule(int year, int month, int day)
-{
-	int cnt = 0;
-	int i;
-    // todo: year, month, day에 해당하는 스케쥴 수 반환
-
-	return cnt;
-}
-
-// 회의실 예약 상태 조회
-void search_schedule(void)
-{
-	int year, month, day;    // 예약 상태를 검색할 날
-	int cnt;                		    // 예약 건 수
-
-	if (login_num == 0)      // 로그인되지 않았다면 반환
-	{
-		printf("@ 로그인 후 사용할 수 있습니다.\n");
-		return;
-	}
-
-	printf("\n[ 회의실 예약 상태 조회 ]\n\n");
-
-	printf("> 년, 월 입력 : ");
-	scanf("%d%d", &year, &month);
-	// todo: 회의실 예약 상태 확인 코드 작성
-
-
-
-
-
-	printf("@ %d건의 예약된 스케줄이 있습니다!\n", cnt);
-
-}
-
-
-//=======< 여기서 부터는 달력 출력 관련 함수 >=================================================
-
-// 윤년 검사 함수
-
-*/
