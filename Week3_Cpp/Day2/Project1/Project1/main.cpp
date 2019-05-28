4a86ddb17dcaad4f8610849c82a5e35e5c436868
DHKim <ultraraptor@naver.com>
2019-05-29 08:22:28 +0900

    190529

--- Week3_Cpp/Day2/Project1/Project1/main.cpp
+++ Week3_Cpp/Day2/Project1/Project1/main.cpp
@@ -5,7 +5,49 @@
 //1332p
 using namespace std;
 
-class 
+class CMyPoint
+{
+public:
+	CMyPoint(int x)
+	{
+
+	cout << "CMyPoint(int)" << endl;
+	if (x > 100)
+		x = 100;
+	m_x = 100;
+	};
+	
+	CMyPoint(int x, int y)
+		:CMyPoint(x)
+	{
+		cout << "CMyPoint(int, int)" << endl;
+
+		if (y > 200)
+			y = 200;
+		m_y = 200;
+	}
+		void Print()
+		{
+			cout << "X : " << m_x << endl;
+			cout << "Y : " << m_y << endl;
+		}
+
+private:
+			int m_x = 0;
+		int m_y = 0;
+	};
+
+	int main(int argc, char ** argv)
+	{
+		CMyPoint ptBegin(110);
+		ptBegin.Print();
+
+
+		CMyPoint ptEnd(50,250);
+		ptEnd.Print();
+		return 0;
+	}
+
 	// */
 
 
