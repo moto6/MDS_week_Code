7c9fe72dc6a31a7c95dd6eaf5fe064298c158e30
DHKim <ultraraptor@naver.com>
2019-05-30 21:02:41 +0900

    a

--- Week3_Cpp/Day4/372p_salary/Yun/salary.cpp
+++ Week3_Cpp/Day4/372p_salary/Yun/salary.cpp
@@ -23,15 +23,81 @@ public://외부에서 사용할 수 있는 함수의 정의 = 인터페이스
 };
 
 class PermanentWorker : public Employee
-{};
+{
+private:
+	int salary;
+public:
+	PermanentWorker(char * name, int money)
+		: Employee(name), salary(money)
+	{}
+	int GetPay() const
+	{
+		return salary;
+	}
+	void ShowSalaryInfo() const
+	{
+		ShowYourName();
+		cout << "salary :" << GetPay() << endl << endl;
+	}
+};
+
 class TemporaryWorker : public Employee
-{};
+{
+private:
+	int workTime;
+	int payPerHour;
+public:
+	TemporaryWorker(char *name, int pay)
+		:Employee(name), workTime(0), payPerHour(pay)
+	{}
+	void AddWorkTime(int time)
+	{
+		workTime += time;
+	}
+
+	int GetPay() const
+	{
+		return workTime*payPerHour;
+	}
+
+	void ShowSalaryInfo() const
+	{
+		ShowYourName();
+		cout << "salary: " << GetPay() << endl << endl;
+	}
+};
+
+
 class SalesWorker : public PermanentWorker 
-{};
+{
+private:
+	int salesResult;
+	double bonusRatio;
+public:
+	SalesWorker(char *name, int money, double ratio)
+		:PermanentWorker(name, money), salesResult(0), bonusRatio(ratio)
+	{}
+	void AddSalesResult(int value)
+	{
+		salesResult += value;
+
+	}
+
+	int GetPay() const
+	{
+		return PermanentWorker::GetPay()
+			+ (int)(salesResult*bonusRatio);
+	}
+	void ShowSalaryInfo() const
+	{
+		ShowYourName();
+		cout << "Salary :" << GetPay() << endl << endl;
+	}
+};
 
 namespace RISK_LEVEL
 {
-	enum { RISK_A = 30, RISK_B = 20, RISK_C = 10; }
+	enum { RISK_A = 30, RISK_B = 20, RISK_C = 10 };
 }
 
 
@@ -40,8 +106,89 @@ class ForeginSalesWorker : public SalesWorker
 private:
 	const int risklevel;
 public:
-	ForeginSalesWorker(char * name)
+	ForeginSalesWorker(char * name, int money, double ratio, int risk)
+		:SalesWorker(name, money, ratio), risklevel(risk)
+	{}
+	
+	int GetRiskPay() const
+	{
+		return (int)(SalesWorker::GetPay() * risklevel / 100.0);
+	}
+
+	int GetPay() const
+	{
+		return SalesWorker::GetPay() + GetRiskPay();
+	}
+
+	void ShowSalaryInfo() const
+	{
+		ShowYourName();
+		cout << "salary :" << SalesWorker::GetPay() << endl;
+		cout << "Riskey Pay :" << GetRiskPay() << endl;
+
+	}
+
 };
+
+
+class EmployeeHandler
+{
+private:
+	Employee* empList[50];
+	int empNum;
+public:
+	EmployeeHandler() : empNum(0)
+	{}
+	void AddEmployee(Employee* emp)
+	{
+		empList[empNum++] = emp;
+	}
+
+	void ShowAllSalaryInfo()const
+	{
+		for (int i = 0; i < empNum; i++)
+			//sum += empList[i]->GetPay();
+			empList[i]->ShowSalaryInfo();
+		//		
+		//cout << "Salary SUM :" << sum << endl;
+	}
+
+	void ShowTotalSalary()const
+	{
+		int sum = 0;
+		///////////////
+		for (int i = 0; i < empNum; i++)
+			sum += empList[i]->GetPay();
+		//////////////
+		cout << "Salary SUM :" << sum << endl;
+	}
+
+
+	~EmployeeHandler()
+	{
+		for (int i = 0; i < empNum; i++)
+			delete empList[i];
+	}
+};
+
+
+int main(void)
+{
+	EmployeeHandler handler;
+
+	ForeginSalesWorker *fseller1
+		= new ForeginSalesWorker("Hong", 1000, 0.1, RISK_LEVEL::RISK_A);
+	fseller1->AddSalesResult(7000);
+	handler.AddEmployee(fseller1);
+
+	ForeginSalesWorker *fseller2
+		= new ForeginSalesWorker("Yoon", 1000, 0.1, RISK_LEVEL::RISK_B);
+	fseller2->AddSalesResult(7000);
+	handler.AddEmployee(fseller2);
+
+	handler.ShowAllSalaryInfo();
+	return 0;
+}
 
 
 
